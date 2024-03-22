99ed0eaa127098d718d571f80520f13dcdc88659
AranzaG <aranza.gavia@gmail.com>
2024-03-22 16:44:30 -0600

    cambios agregados

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,24 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector('#name'); /* Se agregó el # al elemento porque en el html está como clase */
+const $b = document.querySelector('#blog'); 
+const $l = document.querySelector('#location'); /* Se agregó el # a location */
 
-function displayUser(username) {
+// Le coloque el async antes de la funcion
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json(); /* Agregué el data porque quería el json */
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`; /* Se cambió la comilla que estaba anteriormente*/
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` /* Le aguegue el $ porque así se definió la variable */
 }
 
 displayUser('stolinski').catch(handleError);
